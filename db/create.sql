06251bf37ab15d2fb561cafbaae18ca23993932c
Daniel Reina <dannyflatiron@gmail.com>
2020-02-04 03:16:46 +0000

    Automatically backed up by Learn

--- db/create.sql
+++ db/create.sql
@@ -1,1 +1,10 @@
-CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);
+sql = <<-SQL
+CREATE TABLE dogs (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT
+);
+SQL
+
+
+ActiveRecord::Base.connection.execute(sql)
